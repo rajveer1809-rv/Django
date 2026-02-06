5ed5e4f735035203bf0da024dd6d4a328e9be5b3
rajveer1809-rv <ranarajveer65824@gmail.com>
2026-02-06 11:41:29 +0530

    6-2 relation between tables

--- learning26/student/admin.py
+++ learning26/student/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from . models import Student, Product, Car
+from . models import Student, Product, Car, studentdetail, Employee, services, category,Employeedetail,Teacher,subject,Book,LibraryMember
 
 admin.site.register(Student)
 admin.site.register(Product)
 admin.site.register(Car)
-
+admin.site.register(studentdetail)
+admin.site.register(Employee)
+admin.site.register(services)
+admin.site.register(category)
+admin.site.register(Employeedetail)
+admin.site.register(Teacher)
+admin.site.register(subject)
+admin.site.register(Book)
+admin.site.register(LibraryMember)
 # Register your models here.
--- learning26/student/models.py
+++ learning26/student/models.py
@@ -11,6 +11,15 @@ class Student(models.Model):
     class Meta:
         db_table = 'student'
 
+class studentdetail(models.Model):
+    student = models.OneToOneField(Student,on_delete=models.CASCADE)
+    studentaddress = models.CharField(max_length=200)
+    studentqualification = models.CharField(max_length=100)
+    studentexperience = models.IntegerField()
+    
+    class Meta:
+        db_table = 'studentdetail'
+
 class Product(models.Model):
     productname = models.CharField(max_length=100)
     productprice = models.FloatField()
@@ -28,3 +37,89 @@ class Car(models.Model):
     
     class Meta:
         db_table = 'car'
+
+class category(models.Model):
+    categoryname = models.CharField(max_length=100)
+    categorydesc = models.TextField()
+    
+    class Meta:
+        db_table = 'category'
+    def __str__(self):
+        return self.categoryname
+
+class services(models.Model):
+    servicename = models.CharField(max_length=100)
+    service = models.ForeignKey(category,on_delete=models.CASCADE)
+    servicedesc = models.TextField()
+    serviceprice = models.FloatField()
+    
+    class Meta:
+        db_table = 'services'
+    def __str__(self):
+        return self.servicename
+
+class Employee(models.Model):
+    employeename = models.CharField(max_length=100)
+    employeeage = models.IntegerField()
+    employeecity = models.CharField(max_length=100)
+    employeeemail = models.EmailField()
+    
+    class Meta:
+        db_table = 'employee'
+    def __str__(self):
+        return self.employeename
+
+class Employeedetail(models.Model):
+    employee = models.OneToOneField(Employee,on_delete=models.CASCADE)
+    employeeaddress = models.CharField(max_length=200)
+    employeequalification = models.CharField(max_length=100)
+    employeeexperience = models.IntegerField()
+    
+    class Meta:
+        db_table = 'employeedetail'
+    def __str__(self):
+        return self.employee.employeename
+    
+class Teacher(models.Model):
+    teachername = models.CharField(max_length=100)
+    teacherage = models.IntegerField()
+    teachercity = models.CharField(max_length=100)
+    teacheremail = models.EmailField()
+    
+    class Meta:
+        db_table = 'teacher'
+    def __str__(self):
+        return self.teachername
+
+class subject(models.Model):
+    subjectname = models.CharField(max_length=100)
+    subjectteacher = models.ForeignKey(Teacher,on_delete=models.CASCADE)
+    subjectcode = models.CharField(max_length=20)
+    subjectdesc = models.TextField()
+    
+    class Meta:
+        db_table = 'subject'
+    def __str__(self):
+        return self.subjectname
+
+class Book(models.Model):
+    booktitle = models.CharField(max_length=200)
+    bookauthor = models.CharField(max_length=100)
+    bookpublisher = models.CharField(max_length=100)
+    bookprice = models.FloatField()
+    
+    class Meta:
+        db_table = 'book'
+    def __str__(self):
+        return self.booktitle
+
+class LibraryMember(models.Model):
+    membername = models.CharField(max_length=100)
+    member = models.ForeignKey(Book,on_delete=models.CASCADE)
+    memberemail = models.EmailField()
+    memberphone = models.CharField(max_length=15)
+    
+    class Meta:
+        db_table = 'librarymember'
+    def __str__(self):
+        return self.membername
